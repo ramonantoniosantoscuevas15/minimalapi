3f07824fd055916169444686f25ccbd2d40e28e8
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2025-11-08 16:09:52 -0400

    agregando los generos

---
+++ minimalapi/Entidades/Generos.cs
@@ -0,0 +1,8 @@
+﻿namespace minimalapi.Entidades
+{
+    public class Generos
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; } = null!;
+    }
+}
--- minimalapi/Program.cs
+++ minimalapi/Program.cs
@@ -1,6 +1,31 @@
+using minimalapi.Entidades;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/generos",() =>
+{
+    var generos = new List<Generos>
+    {
+        new Generos
+        {
+            Id = 1,
+            Nombre = "Drama"
+        },
+        new Generos
+        {
+            Id = 2,
+            Nombre = "Accion"
+        },
+        new Generos
+        {
+            Id = 3,
+            Nombre = "Comedia"
+        },
+    };
+    return generos;
+});
 
 app.Run();
