4f5cee460b1e97d8888666ec42c3fe0b73f3b0af
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2025-11-10 11:00:21 -0400

    agregando cors

--- minimalapi/Program.cs
+++ minimalapi/Program.cs
@@ -1,11 +1,33 @@
 using minimalapi.Entidades;
 
 var builder = WebApplication.CreateBuilder(args);
+var origenesPermitidos = builder.Configuration.GetValue<String>("origenesPermitidos")!;
+//inicio de servicios
+builder.Services.AddCors(opciones =>
+{ 
+opciones.AddDefaultPolicy(configuracion =>
+
+ {
+    configuracion.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+ });
+    opciones.AddPolicy("Libre", configuracion =>
+    {
+        configuracion.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+
+    });
+});
+
+
+
+//fin de servicios
+
 var app = builder.Build();
+//inicio de area de los middleware
+app.UseCors();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/generos",() =>
+app.MapGet("/generos", () =>
 {
     var generos = new List<Generos>
     {
