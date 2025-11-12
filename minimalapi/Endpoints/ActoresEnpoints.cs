46e126004d42b7263f361d83c70b6eebc7fef895
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2025-11-12 16:24:14 -0400

    almacenando las fotos

--- minimalapi/Endpoints/ActoresEnpoints.cs
+++ minimalapi/Endpoints/ActoresEnpoints.cs
@@ -5,20 +5,29 @@ using Microsoft.AspNetCore.OutputCaching;
 using minimalapi.DTOs;
 using minimalapi.Entidades;
 using minimalapi.Repositorios;
+using minimalapi.Servicios;
 
 namespace minimalapi.Endpoints
 {
     public static class ActoresEnpoints
     {
+        private static readonly string contenedor = "actores";
         public static RouteGroupBuilder MapActores(this RouteGroupBuilder group) 
         {
             group.MapPost("/",Crear).DisableAntiforgery();
             return group;
         }
         static async Task<Created<ActorDTO>> Crear([FromForm] CrearActorDTO crearActorDTO, 
-            IRepositorioActores repositorio, IOutputCacheStore outputCacheStore, IMapper mapper )
+            IRepositorioActores repositorio, IOutputCacheStore outputCacheStore, IMapper mapper,
+            IAlmacenadorArchivos almacenadorArchivos
+            )
         {
             var actor = mapper.Map<Actor>( crearActorDTO );
+            if (crearActorDTO.Foto is not null) 
+            {
+                var url = await almacenadorArchivos.Almacenar(contenedor, crearActorDTO.Foto);
+                actor.Foto = url;
+            }
             var id = await repositorio.Crear( actor );
             await outputCacheStore.EvictByTagAsync("actores-get", default);
             var actorDTO = mapper.Map<ActorDTO>(actor);
---
+++ minimalapi/Servicios/IAlmacenadorArchivos.cs
@@ -0,0 +1,13 @@
+﻿namespace minimalapi.Servicios
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task Borrar(string? ruta, string contenedor);
+        Task<string> Almacenar(string contendor, IFormFile archivo);
+        async Task<string> Editar(string ruta, string contenedor,IFormFile archivo)
+        {
+            await Borrar(ruta, contenedor);
+            return await Almacenar(contenedor, archivo);
+        }
+    }
+}
